8243480945d1644dee63467ac1cdf29396bf0409
steinhardt21 <alexandru.enache@protonmail.com>
2024-07-12 19:06:04 +0300

    feat: Add NotFound component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button' 
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link className={buttonVariants({ variant: "default"})} href="/">Return Home</Link>
+    </div>
+  )
+}
