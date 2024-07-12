8a364435e709a1f623837dfd0ed602e79c404dd7
steinhardt21 <alexandru.enache@protonmail.com>
2024-07-12 19:10:28 +0300

    feat: Add RadialChart component for displaying parking availability

---
+++ src/components/radial-chart-parking.tsx
@@ -0,0 +1,84 @@
+"use client"
+
+import {
+  Label,
+  PolarGrid,
+  PolarRadiusAxis,
+  RadialBar,
+  RadialBarChart,
+} from "recharts"
+
+import { ChartConfig, ChartContainer } from "@/components/ui/chart"
+
+const chartConfig = {
+  spots: {
+    label: "spots",
+  },
+  available: {
+    label: "available",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig
+
+export function RadialChart({ availableSpots, totalSpots }: {availableSpots: number, totalSpots: number }) {
+  const endAngle = 360 * (availableSpots / totalSpots)
+  const chartData = [
+    { spots: availableSpots, fill: "var(--color-available)" },
+  ]
+  
+  return (
+    <ChartContainer
+      config={chartConfig}
+      className="mx-auto aspect-square max-h-[250px]"
+    >
+      <RadialBarChart
+        data={chartData}
+        startAngle={0}
+        endAngle={endAngle}
+        innerRadius={80}
+        outerRadius={110}
+      >
+        <PolarGrid
+          gridType="circle"
+          radialLines={false}
+          stroke="none"
+          className="first:fill-muted last:fill-background"
+          polarRadius={[86, 74]}
+        />
+        <RadialBar dataKey="spots" background cornerRadius={10} />
+        <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+          <Label
+            content={({ viewBox }) => {
+              if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                return (
+                  <text
+                    x={viewBox.cx}
+                    y={viewBox.cy}
+                    textAnchor="middle"
+                    dominantBaseline="middle"
+                  >
+                    <tspan
+                      x={viewBox.cx}
+                      y={viewBox.cy}
+                      className="fill-foreground text-4xl font-bold"
+                    >
+                      {chartData[0].spots.toLocaleString()}
+                    </tspan>
+                    <tspan
+                      x={viewBox.cx}
+                      y={(viewBox.cy || 0) + 24}
+                      className="fill-muted-foreground"
+                    >
+                      Available spots
+                    </tspan>
+                  </text>
+                )
+              }
+            }}
+          />
+        </PolarRadiusAxis>
+      </RadialBarChart>
+    </ChartContainer>
+      
+  )
+}
