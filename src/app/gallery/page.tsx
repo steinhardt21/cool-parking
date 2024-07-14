21c492c8f7245887535e7b5d11e2f1fc87b12f38
steinhardt21 <alexandru.enache@protonmail.com>
2024-07-14 21:26:34 +0300

    chore: Added prefetch for links

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -9,8 +9,8 @@ export default function ParkingsGalleryPage() {
       <h1 tabIndex={0} className="font-heading text-3xl md:text-4xl">Parkings gallery</h1>
       <section className="grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-4 sm:gap-3">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-1">
-          {imagesDemo.demo.map((image, index) => (
-            <Link key={index} href={`/gallery/${image.name}`}>
+          {imagesDemo.demo.map(image => (
+            <Link prefetch key={image.id} href={`/gallery/${image.name}`}>
               <Image style={{ viewTransitionName: `image-${image.name}`}} src={image.src} alt="parking lot" width={500} height={500} />
             </Link>
           ))}
