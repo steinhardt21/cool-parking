0d1a56adb797596609517d2ae7d256414da2adfd
steinhardt21 <alexandru.enache@protonmail.com>
2024-07-14 20:05:16 +0300

    chore: Add RefreshCache component for automatic cache refresh on focus

--- src/components/refresh-cache.tsx
+++ src/components/refresh-cache.tsx
@@ -1,15 +1,29 @@
 'use client'
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { useInterval } from "../hooks/use-interval"
 
 export function RefreshCache({ refresh }: { refresh: () => Promise<void> }) {
+  const [shouldRun, setShouldRun] = useState<boolean>(
+    typeof document !== 'undefined' && document.hasFocus()
+  )
  
   useEffect(() => {
-    const onFocus = () => refresh()
+    const onFocus = () => {
+      refresh()
+      setShouldRun(true)
+    }
+    const onBlur = () => setShouldRun(false)
+
     window.addEventListener('focus', onFocus)
+    window.addEventListener('blur', onBlur)
 
-    return () => window.removeEventListener('focus', onFocus)
-  }, [refresh])
+    return () => {
+      window.removeEventListener('focus', onFocus)
+      window.removeEventListener('blur', onBlur)
+    }
+  })
 
+  useInterval(refresh, shouldRun ? 3000 : null)
   return null
 }
